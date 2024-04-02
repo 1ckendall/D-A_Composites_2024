9b3628000a159c176e298b54cebdf57ab2672cf9
ndighe <ndighe2002@gmail.com>
2024-04-02 16:28:25 +0200

    Update main.py

--- Part1/main.py
+++ Part1/main.py
@@ -3,17 +3,20 @@ import numpy as np
 from pprint import pprint
 from Class import Lamina, Laminate
 
+# plot controls (Booleans)
+plot_1a = False # plot 1a: engineering constants for laminae (default = True)
+
 # UD Lamina Mean Properties 
-E1 = 145.3E3 # [MPa]
-E2 = 8.5E3 # [MPa]
-v12 = 0.31 # [--]
-G12 = 4.58E3 # [MPa]
-Xt = 1932 # [MPa]
-Yt = 108 # [MPa]
-Xc = 1480 # [MPa]
-Yc = 220 # [MPa]
-S = 132.8 # [MPa]
-t= 0.125 # [mm] # free variable, can be changed 
+E1 = 147E9# 145.3E9 # [Pa]
+E2 = 10.3E9#8.5E9 # [Pa]
+v12 = 0.27#0.31 # [--]
+G12 = 7e9#4.58E9 # [Pa]
+Xt = 1932E6 # [Pa]
+Yt = 108E6 # [Pa]
+Xc = 1480E6 # [Pa]
+Yc = 220E6 # [Pa]
+S = 132.8E6 # [Pa]
+t= 0.125E-3 # [m] # free variable, can be changed 
 
 # UD Lamina Std. Dev Properties 
 pass
@@ -31,6 +34,12 @@ Ey_arr = np.zeros((len(n_arr), len(theta_arr)))
 Gxy_arr = np.zeros((len(n_arr), len(theta_arr)))
 vxy_arr = np.zeros((len(n_arr), len(theta_arr)))
 vyx_arr = np.zeros((len(n_arr), len(theta_arr)))
+
+Exb_arr = np.zeros((len(n_arr), len(theta_arr)))
+Eyb_arr = np.zeros((len(n_arr), len(theta_arr)))
+Gxyb_arr = np.zeros((len(n_arr), len(theta_arr)))
+vxyb_arr = np.zeros((len(n_arr), len(theta_arr)))
+vyxb_arr = np.zeros((len(n_arr), len(theta_arr)))
 
 # simulation loop
 for i in range(len(n_arr)):
@@ -54,33 +63,112 @@ for i in range(len(n_arr)):
         vxy_arr[i, j] = Laminate_ij.vxy
         vyx_arr[i, j] = Laminate_ij.vyx
         
+        Exb_arr[i, j] = Laminate_ij.Exb
+        Eyb_arr[i, j] = Laminate_ij.Eyb
+        Gxyb_arr[i, j] = Laminate_ij.Gxyb
+        vxyb_arr[i, j] = Laminate_ij.vxyb
+        vyxb_arr[i, j] = Laminate_ij.vyxb
         
-# Create a figure and subplots
-plt.figure('1')
-plt.clf()
-for i in range(len(n_arr)):
-    plt.plot(theta_arr, Ex_arr[i, :] , color='blue', label = 'Ex')
-    plt.plot(theta_arr, Ey_arr[i, :] , color='red', label = 'Ey')
-    plt.plot(theta_arr, Gxy_arr[i, :] , color='green', label = 'Gxy')
-plt.grid()
-plt.legend()
-plt.title('Ex, Ey, Gxy')
-plt.xlabel(r'$\theta$')
-plt.ylabel('[MPa]')
+if plot_1a:      
+    fig, axes = plt.subplots(2, 2, figsize=(10, 8))
+    # Plot 1: Inplane Engineering Stiffness
+    for i in range(len(n_arr)):
+        if i == 0:
+            axes[0, 0].plot(theta_arr, Ex_arr[i, :], color='blue', label=r'$E_{x}$')
+            axes[0, 0].plot(theta_arr, Ey_arr[i, :], color='red', label=r'$E_{y}$')
+            axes[0, 0].plot(theta_arr, Gxy_arr[i, :], color='green', label=r'$G_{xy}$')
+            if np.allclose(Ex_arr[1:,:], Ex_arr[:-1,:]):
+                text2plot = 'n = ' + ','.join(map(str, n_arr))
+                axes[0, 0].text(60, 0.4*np.max(Ex_arr), text2plot)
+            if np.allclose(Ey_arr[1:,:], Ey_arr[:-1,:]):
+                text2plot = 'n = ' + ','.join(map(str, n_arr))
+                axes[0, 0].text(60, 0.9*np.max(Ey_arr), text2plot)
+            if np.allclose(Gxy_arr[1:,:], Gxy_arr[:-1,:]):
+                text2plot = 'n = ' + ','.join(map(str, n_arr))
+                axes[0, 0].text(50, np.max(Gxy_arr), text2plot)
+        else:
+            axes[0, 0].plot(theta_arr, Ex_arr[i, :], color='blue')
+            axes[0, 0].plot(theta_arr, Ey_arr[i, :], color='red')
+            axes[0, 0].plot(theta_arr, Gxy_arr[i, :], color='green')  
+    axes[0, 0].grid()
+    axes[0, 0].legend()
+    axes[0, 0].set_title('Inplane Stiffness')
+    axes[0, 0].set_xlabel(r'$\theta$ [deg]')
+    axes[0, 0].set_ylabel('[Pa]')
+    
+    # Plot 2: Inplane Engineering Poisson Ratio
+    for i in range(len(n_arr)):
+        if i == 0:
+            axes[0, 1].plot(theta_arr, vxy_arr[i, :], color='red', label=r'$\nu_{xy}$')
+            axes[0, 1].plot(theta_arr, vyx_arr[i, :], color='blue', label=r'$\nu_{yx}$')
+            if np.allclose(vxy_arr[1:,:], vxy_arr[:-1,:]):
+                text2plot = 'n = ' + ','.join(map(str, n_arr))
+                axes[0, 1].text(40, np.max(vxy_arr), text2plot)
+            if np.allclose(vyx_arr[1:,:], vyx_arr[:-1,:]):
+                text2plot = 'n = ' + ','.join(map(str, n_arr))
+                axes[0, 1].text(30, 0.9*np.max(vyx_arr), text2plot)
+        else:
+            axes[0, 1].plot(theta_arr, vxy_arr[i, :], color='red')
+            axes[0, 1].plot(theta_arr, vyx_arr[i, :], color='blue')
+    axes[0, 1].grid()
+    axes[0, 1].legend()
+    axes[0, 1].set_title('Inplane Poisson Ratio')
+    axes[0, 1].set_xlabel(r'$\theta [deg]$')
+    axes[0, 1].set_ylabel('[-]')
+    
+    # Plot 3: Flexural Engineering Stiffness
+    for i in range(len(n_arr)):
+        if i == 0:
+            axes[1, 0].plot(theta_arr, Exb_arr[i, :], color='blue', label=r'$E_{xb}$')
+            axes[1, 0].plot(theta_arr, Eyb_arr[i, :], color='red', label=r'$E_{yb}$')
+            axes[1, 0].plot(theta_arr, Gxyb_arr[i, :], color='green', label=r'$G_{xyb}$')
+        else:
+            axes[1, 0].plot(theta_arr, Exb_arr[i, :], color='blue')
+            axes[1, 0].plot(theta_arr, Eyb_arr[i, :], color='red')
+            axes[1, 0].plot(theta_arr, Gxyb_arr[i, :], color='green')
+        text2plot = f'n = {n_arr[i]}'
+        axes[1, 0].text(theta_arr[np.argmax(Exb_arr[i, :])], np.max(Exb_arr[i, :]), text2plot)
+        axes[1, 0].text(theta_arr[np.argmax(Eyb_arr[i, :])], np.max(Eyb_arr[i, :]), text2plot)
+        axes[1, 0].text(theta_arr[np.argmax(Gxyb_arr[i, :])], np.max(Gxyb_arr[i, :]), text2plot)
+    axes[1, 0].grid()
+    axes[1, 0].legend()
+    axes[1, 0].set_title('Flexural Stiffness')
+    axes[1, 0].set_xlabel(r'$\theta [deg]$')
+    axes[1, 0].set_ylabel('[Pa]')
+    
+    # Plot 4: Flexural Engineering Poisson Ratio
+    for i in range(len(n_arr)):
+        if i == 0:
+            axes[1, 1].plot(theta_arr, vxyb_arr[i, :], color='red', label=r'$\nu_{xyb}$')
+            axes[1, 1].plot(theta_arr, vyxb_arr[i, :], color='blue', label=r'$\nu_{yxb}$')
+        else:
+            axes[1, 1].plot(theta_arr, vxyb_arr[i, :], color='red')
+            axes[1, 1].plot(theta_arr, vyxb_arr[i, :], color='blue')
+        text2plot = f'n = {n_arr[i]}'
+        axes[1, 1].text(theta_arr[np.argmax(vxyb_arr[i, :])], np.max(vxyb_arr[i, :]), text2plot)
+        axes[1, 1].text(theta_arr[np.argmax(vyxb_arr[i, :])], np.max(vyxb_arr[i, :]), text2plot)
+    axes[1, 1].grid()
+    axes[1, 1].legend()
+    axes[1, 1].set_title('Flexural Poisson Ratio')
+    axes[1, 1].set_xlabel(r'$\theta [deg]$')
+    axes[1, 1].set_ylabel('[-]')
+    plt.tight_layout()
+    plt.show()
 
-plt.figure('2')
-plt.clf()
-for i in range(len(n_arr)):
-    plt.plot(theta_arr, vxy_arr[i, :] ,color='red', label = 'vxy')
-    plt.plot(theta_arr, vyx_arr[i, :] ,color='blue', label = 'vyx')
-plt.grid()
-plt.legend()
-plt.title('vxy, vyx')
-plt.xlabel(r'$\theta$')
-plt.ylabel('[-]')
+# Question 1b: Lamina Stress and Strain
+Nx = 0.2E2 # [N/m]
+Ny = 1.8E4 # [N/m]
+Mx = 18E3 # [N]
 
-plt.show()
+layup =  [0, 30, -30, 90] #[0, 0, 90, 30, 90]  #transverse symmetric: [90, 30, 90, 0, 0, 0, 0, 90, 30, 90] # symmetric: [0, 0, 90, 30, 90, 90, 30, 90, 0, 0]  # truncated: [0, 0, 90, 30, 90] 
+plylist = []
 
+for angle in layup:
+    plylist.append(Lamina(angle,E1,E2,G12,v12,Xt,Xc,Yt,Yc,S,t))
+
+Laminate = Laminate(plylist, Nx=Nx, Ny=Ny, Ns=0, Mx=Mx, My=0, Ms=0)
+lamina_strain, lamina_stress = Laminate.getStressStrain()
+print(f'Strain: {lamina_strain}\nStress: {lamina_stress}')
 # if __name__ == "__main__":
 #     print("Running Main")
 #     E1 = 140

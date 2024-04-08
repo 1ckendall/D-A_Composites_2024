f1fc24c0c559214bfae067c9914c589b80b02c5e
ndighe <ndighe2002@gmail.com>
2024-04-08 12:43:22 +0200

    Update Q3_Reliability.py

--- Part1/Q3_Reliability.py
+++ Part1/Q3_Reliability.py
@@ -4,6 +4,10 @@ from scipy.stats import norm
 from pprint import pprint
 from Class import Lamina, Laminate
 
+import time
+
+start_time = time.time()
+
 # plot controls (Booleans)
 
 # savefig controls
@@ -32,6 +36,22 @@ def inverse_transform_sampling(samples, cdf, num_points=1):
     interpolated_samples = np.interp(u, cdf, sorted_samples)
     
     return interpolated_samples
+
+'''
+Index of each property variable in UD array
+Property | Index
+------------------
+E1       | 0
+E2       | 1
+v12      | 2
+G12      | 3
+Xt       | 4
+Xc       | 5
+Yt       | 6
+Yc       | 7
+S        | 8
+t        | 9       
+'''
 
 # UD Lamina Mean Properties 
 E1_mean = 145.3E9 # [Pa]
@@ -66,30 +86,34 @@ UD_std = np.array((E1_std, E2_std, v12_std, G12_std, Xt_std, Yt_std, Xc_std, Yc_
 # Question 3: Reliability Analysis
 
 layup =  [0, 90, +45, -45, - 45, + 45, 90, 0, 0, 90, +45, -45, - 45, + 45, 90, 0] # laminate [0/90/±45]_2s
-N_load = 850000000 # 1250 [N], applied load
+# benchmark N_load for MaxStress
+# N_load = 1.20e6 # 1.5e6 (pf = 0.0) # 2e6 (pf = 0.0) # 3e6 (pf = 0.31027094717668485) # 3.4e6 # (pf = 0.3610857) # 3.4375e6 (pf = 0.9715166666666667)  # 1.2e6 [N/m] (pf = 0.0003) # 0.85e6 [N/m] (pf = 0), applied load # INCORRECT AS ALL FAILURE MODES (INCLD. DAMAGE) WERE CONSIDERED FAILURE
+
+# benchmark N_load for Puck
+N_load = 0.6e6  # 0.6e6 (pf =  0.35666666666666663) # 0.7e6 (pf =  1.0) # 0.5e6 (pf =  0.0) # 0.1e6 (pf =  0.0) # 1e6 (pf =  1.0) # 2e6 (pf =  1.0) # 3e6 (pf =  1.0) # 1.2e6 [N/m] (pf = 0.0003) # 0.85e6 [N/m] (pf = 0), applied load
+
 theta = 30 # [deg], inclination of the load vector w.r.t. x-axis
 Nx = N_load * np.cos(np.radians(theta)) # [N]
 Ny = N_load * np.cos(np.radians(theta)) # [N]
 
 n_vars = len(UD_mean) # number of independent, Gaussian random variables 
-iterations = 100 # 1E8 Monte Carlo: number of rounds of simulations (R)
-N_max = 10 # maximum number of simulations per round (N)
+iterations = 3 # 1E8 Monte Carlo: number of rounds of simulations (R)
+N_max = 100 # maximum number of simulations per round (N)
 Pf_arr = np.zeros((n_vars, iterations)) # array for probabability of failure, registered for each round of simulations and for every random variable
-
 
 # simulation loop
 UD = UD_mean
 loop_counter = 0 
 # loop through all random variables 
-# for i in range(n_vars): # full simulation
-for i in range(0, 1): # testing (only treat E1 as random variable)
+for i in range(n_vars): # full simulation
+#for i in range(0, 1): # testing (only treat E1 as random variable)
     samples, cdf = generate_cdf(mean = UD_mean[i], std_dev = UD_std[i])#, num_points = iterations)
     # loop through rounds of simulations (R)
     for j in range(iterations): 
         firstplyfailureoccurence = False
         N = 0
         # determine number of simulations (N) needed for failure 
-        while not firstplyfailureoccurence and N < (N_max):
+        while firstplyfailureoccurence == False and N < (N_max):
             N += 1
             loop_counter += 1
             # sample random variable from CDF
@@ -100,22 +124,43 @@ for i in range(0, 1): # testing (only treat E1 as random variable)
             for angle in layup:
                 # plylist_ijk.append(Lamina(angle,E1,E2,G12,v12,Xt,Xc,Yt,Yc,S,t))
                 plylist_ijk.append(Lamina(angle, *UD))
-            Laminate_ijk = Laminate(plylist_ijk)
+            Laminate_ijk = Laminate(plylist_ijk, Nx=Nx, Ny=Ny, Ns=0, Mx=0, My=0, Ms=0)
             
             Laminate_ijk.getStressStrain()
             
+            failuretracking = 0 
             # checking for failure per lamina. Output: update boolean 'firstplyfailureoccurence'
             for idx, ply in enumerate(plylist_ijk):
                 # assign computed stresses as lamina attributes
                 ply.sigma_1 = Laminate_ijk.sigma11[idx]
                 ply.sigma_2 = Laminate_ijk.sigma22[idx]
-                ply.tau_21 =Laminate_ijk.sigma12[idx]
+                ply.tau_21 = Laminate_ijk.sigma12[idx]
                 
-                # Maximum stress failure criterion (Placeholder)
-                ply.maxStressFibreFail() #fiber failure
-                ply.maxStressInterFibreFail() #InterFiberfailure
-                ply.maxStressShearFail() #Shearfailure
-                    
+                # print(f'Laminate_ijk.sigma11: {Laminate_ijk.sigma11[idx]}, Laminate_ijk.sigma22: {Laminate_ijk.sigma22[idx]}, Laminate_ijk.sigma12: {Laminate_ijk.sigma12[idx]}')
+                # print(f'ply sigma_1: {ply.sigma_1}, ply.sigma_2: {ply.sigma_2}, ply.tau_21: {ply.tau_21}')
+                
+                # # Maximum stress failure criterion (Placeholder)
+                # ply.maxStressFibreFail() #fiber failure
+                # ply.maxStressInterFibreFail() #InterFiberfailure
+                # ply.maxStressShearFail() #Shearfailure
+                
+                # if ply.failuremode == "Tensile Fibre" or ply.failuremode == "Compressive":
+                #   failuretracking = 2
+                # elif ply.failuremode == "Tensile Inter-Fibre" or ply.failuremode=="Compressive Inter-Fibre" or ply.failuremode ==  "Shear Parallel to Fibres":
+                #   failuretracking += 1
+                
+                
+                
+                # Puck failure criterion
+                
+                ply.PuckFibreFail(sigma_1 = ply.sigma_1, sigma_2 = ply.sigma_2, sigma_3 = ply.tau_21, R_para_t = UD[4], R_para_c = UD[5], v_perppara = UD[2], E_para = UD[0])  #Puck fiber failure #### currently using Minor poisson, to make it consistent with Minor poisson at failure
+                ply.PuckIFF(sigma_22 = ply.sigma_2, sigma_21 = ply.tau_21 , sigma_22T_u = UD[6], sigma_22C_u=UD[7], sigma_12_u=UD[8])  #Puck fiber failure #### currently using Minor poisson, to make it consistent with Minor poisson at failure          
+                
+                if  ply.failuremode == "FFT" or ply.failuremode == "FFC":
+                    failuretracking = 2
+                elif ply.failuremode == 'IFF A' or ply.failuremode == "IFF B" or ply.failuremode == "IFF C":
+                    failuretracking += 1
+                
                 
                   # Puck failure criterion
             #     ply.PuckFibreFail(sigma_2, gamma_21, epsilon1T, epsilon1C, epsilon1)
@@ -124,18 +169,24 @@ for i in range(0, 1): # testing (only treat E1 as random variable)
             #     tau_21c = ply.get_tau_21c(S21, p_perpperp_minus)
             #     ply.PuckIFF(sigma_2, sigma_1, sigma_1D, tau_21, tau_21c, S21, RAperpperp, p_perppara_plus, p_perppara_minus, p_perpperp_minus, Y_T, Y_C)
            
-   
-                # FPF Condition (any failure is considered FPF)
-                if ply.failuremode == 'Tensile Fibre' or ply.failuremode == 'Compressive' or ply.failuremode == 'Tensile Inter-Fibre' or ply.failuremode=="Compressive Inter-Fibre" or ply.failuremode ==  "Shear Parallel to Fibres":
+                # FPF Condition
                 # if ply.failuremode is not None:    
+                # Max Stress: (any failure is considered FPF) # INCORRECT
+                # if ply.failuremode == "Tensile Fibre" or ply.failuremode == "Compressive" or ply.failuremode == "Tensile Inter-Fibre" or ply.failuremode=="Compressive Inter-Fibre" or ply.failuremode ==  "Shear Parallel to Fibres":
+                    
+                if failuretracking == 2:
                     firstplyfailureoccurence = True
                     Pf_arr[i,j] = 1/N
-            print(f'loop count: {loop_counter}, i = {i}, j = {j}, k = {N} ')
+            print(f'loop count: {loop_counter}, i = {i}, j = {j}, k = {N}, isFPF?: {firstplyfailureoccurence} ')
 
-Pf = np.average(Pf_arr)
-print(f'\nLoad: {N_load}[N] => Probability of Failure: {Pf}')
-    
-
+Pf = np.mean(Pf_arr)
+print(f'\nLoad: {N_load}[N/m] => Probability of Failure: {Pf}')
 
 
+# Record end time
+end_time = time.time()
 
+# Compute elapsed time
+elapsed_time = end_time - start_time
+
+print("Time taken:", elapsed_time, "seconds")

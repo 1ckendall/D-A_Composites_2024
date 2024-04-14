a0ac344056b1c55d7b276c030a2884e1194baffa
ndighe <ndighe2002@gmail.com>
2024-04-14 18:39:57 +0200

    Update 1_aluminum.py

--- Part2/1_aluminum.py
+++ Part2/1_aluminum.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Aluminum material properties 
 Ex = 69*10**9 # [Pa]
@@ -29,9 +30,9 @@ R_outer = D_outer/2 # [m]
 R_inner = D_inner/2 # [m]
 
 # area moment of inertia (circular; thin walled assumption NOT made)
-I_xx = np.pi * (D_outer**4 - D_inner**4) / 64 # [m]
-I_yy = I_xx # [m] # assume 
-
+# I_xx = np.pi * (D_outer**4 - D_inner**4) / 64 # [m]
+# I_yy = I_xx # [m] # assume 
+sf_global = 1.5
 sf_bending = 1.5 # safety factor source: Airframe Stress Analysis and Sizing
 sf_shear = 1.5 # safety factor source: Airframe Stress Analysis and Sizing
 
@@ -44,24 +45,44 @@ def calc_thickness_for_bending(M,R,Yt,sf,rho): # Momnent, Outer Radius, yeild st
   weight_per_length = rho*2*np.pi*R*t
   return t,weight_per_length
 
-thickness, wt = calc_thickness_for_bending(M = M_x, R = R_outer, Yt = Xt, sf = sf_bending, rho = rho) 
+thickness_bending, wt = calc_thickness_for_bending(M = M_x, R = R_outer, Yt = Xt, sf = sf_bending, rho = rho) 
 
-print(f"Bending thickness in m = {thickness}")
+print(f"Bending thickness in m = {thickness_bending}")
 print(f"weight per length ={wt}")
 
 
 # Shear 
 def calc_thickness_for_shear(V, S, R, sf, rho, theta = np.pi/2):
-    qs = V/(np.pi)*R*np.sin(theta)
+    qs = V/(np.pi*R)*np.sin(theta)
     tau_allow = S/sf
     t = qs/tau_allow
     weight_per_length = rho*2*np.pi*R*t
     return t,weight_per_length
 
-thickness, wt = calc_thickness_for_shear(V = V_y, S = S, R = R_outer, sf = sf_shear, rho = rho, theta = np.pi/2)
-print(f"Shear thickness in m = {thickness}")
+thickness_shear, wt = calc_thickness_for_shear(V = V_y, S = S, R = R_outer, sf = sf_shear, rho = rho, theta = np.pi/2)
+print(f"Shear thickness in m = {thickness_shear}")
 print(f"weight per length ={wt}")
 
+Ixx = np.pi*R_outer**3*t
+Iyy = Ixx
+y = np.linspace(0, R_outer, 4)
+t_arr = np.ones(len(y))*thickness_bending
+sigma_z = np.zeros(len(y))
+tau_xy = np.zeros(len(y))
+
+def vonMises(sigma_z, tau_xy):
+    return np.sqrt(sigma_z**2 + 3*tau_xy**2)
+
+sigma_a = Xt/sf_global # Yt taken as minimum
+for i in range(len(y)):
+    qs = V_y/(np.pi*R_outer)*np.sqrt(1-(y[i]/R_outer)**2)
+    t_arr[i] =   1/sigma_a * np.sqrt((M_x*y[i]/(np.pi*R_outer**3))**2 + 3*qs**2)  
+    print(f'y = {y[i]} m, t = {t_arr[i]}m')
+    
+plt.plot(y, t_arr, marker ='o')
+plt.show()
+    
+    
 
 # Bucklings
 

13b322aed43a31369610eb496d4b8c940572c0d7
Aranshumodi <aranshu.modi@gmail.com>
2024-04-18 10:44:10 +0200

    Update Test_discritization.py

--- Part2/Test_discritization.py
+++ Part2/Test_discritization.py
@@ -21,20 +21,11 @@ t_ply = 0.135*10**-3 # [m]
 # y: yaw (+ve pointing upwards), 
 # z: roll (+ve pointing towards nose)
 
-# Geometry
-D_outer = 6 # [m]
-t = 0.002 # [m] TODO: placeholde
-D_inner = D_outer - 2*t # [m]
-R_outer = D_outer/2 # [m]
-R_inner = D_inner/2 # [m]
-Ixx_t=np.pi*(R_outer**3) # Ixx / thickness(unknown)
-
-
 #loading parameters
 M_x = 15000000 # [Nm]
 V_y = 1500000 # [N]
 
-# Loading 
+# ----------------------------------------------------------------------------------Functions --------------------------------------------------------
 def calc_loadvector(V, M, n_points=4):  
     y = np.linspace(0, R_outer, n_points)    
     sigma_z_times_t_arr = np.zeros(len(y))
@@ -56,11 +47,9 @@ def create_circle(num_points,R):
     y = R*np.sin(Side_boom)
 
 
-    arc_lengths = np.zeros_like(x)
-    for i in range(1, num_points):
-        arc_lengths[i] = 2*np.pi*R/(num_points-1) #np.sqrt((x[i] - x[i-1])**2 + (y[i] - y[i-1])**2)
+    arc_length = 2*np.pi*R/(num_points-1) #np.sqrt((x[i] - x[i-1])**2 + (y[i] - y[i-1])**2)
 
-    interp_points = 100  # Number of points after interpolation
+    interp_points = 1000  # Number of points after interpolation
     spline = CubicSpline(np.arange(num_points), np.column_stack([x, y]), axis=0)
     interp_indices = np.linspace(0, num_points - 1, interp_points)
     interp_circle = spline(interp_indices)
@@ -71,52 +60,198 @@ def create_circle(num_points,R):
 
 
     plt.figure(figsize=(8,8))
-    plt.plot(x,y,'bo-')
-    #plt.scatter(X[0],Y[0],color='red',marker='x')
+    plt.plot(x,y)#,'bo-'
     plt.axis('equal')
     plt.grid()
     plt.plot(interp_x, interp_y)  # Plot the circle with blue dots connected by lines
     
-    return x, y, arc_lengths, X,Y
+    return x, y, arc_length, X,Y
 
-num_points=8
+
+#--------------------------------------------------------------Begin here-----------------------------------------------
+# Geometry
+D_outer = 6 # [m]
+t = 0.002 # [m] TODO: placeholde
+D_inner = D_outer - 2*t # [m]
+R_outer = D_outer/2 # [m]
+R_inner = D_inner/2 # [m]
+Ixx_t=np.pi*(R_outer**3) # Ixx / thickness(unknown)
+
+
+#-------------------------------------------------------------skin parameters-------------------------------------------
+
+# skin parameters
+Eskin_val=[(142*10**9),(142*10**9),(142*10**9)]
+T_skin=[0.002,0.002,0.002]
+
+#stiffner parameters
+A_stiffner=[0,0,0,0]
+Estiffner_val=[0,0,0,0]
+
+#------------------------------------------------------------ Initializing----------------------------------------------
+num_points=144
 x,y,arc_length,X,Y=create_circle(num_points+1,3)
 x=x[:-1]
 y=y[:-1]
 X=X[:-1]
 Y=Y[:-1]
-arc_length=arc_length[1:]
+#print(arc_length)
 
-# calculate stress due to moment on the discritised length
-X[0],Y[0]
-x[0],y[0]
-x[-1],y[-1]
+# Panel node breakdown
+Area_panel= [arc_length*t for t in T_skin]
+Panel_Areas=[]
+Panel_Areas.extend([Area_panel[0]]*9)
+Panel_Areas.extend([Area_panel[1]]*18)
+Panel_Areas.extend([Area_panel[2]]*18)
+Panel_Areas.extend([Area_panel[1]]*18)
+Panel_Areas.extend([Area_panel[0]]*18)
+Panel_Areas.extend([Area_panel[1]]*18)
+Panel_Areas.extend([Area_panel[2]]*18)
+Panel_Areas.extend([Area_panel[1]]*18)
+Panel_Areas.extend([Area_panel[0]]*9)
 
-A_stiffner=[0]
+# thickness array
+T_skinarr=[]
+T_skinarr.extend([T_skin[0]]*9)
+T_skinarr.extend([T_skin[1]]*18)
+T_skinarr.extend([T_skin[2]]*18)
+T_skinarr.extend([T_skin[1]]*18)
+T_skinarr.extend([T_skin[0]]*18)
+T_skinarr.extend([T_skin[1]]*18)
+T_skinarr.extend([T_skin[2]]*18)
+T_skinarr.extend([T_skin[1]]*18)
+T_skinarr.extend([T_skin[0]]*9)
+
+# Elasticity Array
+E_skin=[]
+E_skin.extend([Eskin_val[0]]*9)
+E_skin.extend([Eskin_val[1]]*18)
+E_skin.extend([Eskin_val[2]]*18)
+E_skin.extend([Eskin_val[1]]*18)
+E_skin.extend([Eskin_val[0]]*18)
+E_skin.extend([Eskin_val[1]]*18)
+E_skin.extend([Eskin_val[2]]*18)
+E_skin.extend([Eskin_val[1]]*18)
+E_skin.extend([Eskin_val[0]]*9)
+
+# Stiffner Area Addition
+stiff_loc=[]
+stiff_loc.extend([A_stiffner[0]]*8)
+stiff_loc.extend([A_stiffner[0]])
+stiff_loc.extend([A_stiffner[0]]*17)
+stiff_loc.extend([A_stiffner[0]])
+stiff_loc.extend([A_stiffner[0]]*17)
+stiff_loc.extend([A_stiffner[0]])
+stiff_loc.extend([A_stiffner[0]]*17)
+stiff_loc.extend([A_stiffner[0]])
+stiff_loc.extend([A_stiffner[0]]*17)
+stiff_loc.extend([A_stiffner[0]])
+stiff_loc.extend([A_stiffner[0]]*17)
+stiff_loc.extend([A_stiffner[0]])
+stiff_loc.extend([A_stiffner[0]]*17)
+stiff_loc.extend([A_stiffner[0]])
+stiff_loc.extend([A_stiffner[0]]*17)
+stiff_loc.extend([A_stiffner[0]])
+stiff_loc.extend([A_stiffner[0]]*9)
+
+# Elasticity matrices
+E_stiffner=[]
+E_stiffner.extend([Estiffner_val[0]]*8)
+E_stiffner.extend([Estiffner_val[0]])
+E_stiffner.extend([Estiffner_val[0]]*17)
+E_stiffner.extend([Estiffner_val[0]])
+E_stiffner.extend([Estiffner_val[0]]*17)
+E_stiffner.extend([Estiffner_val[0]])
+E_stiffner.extend([Estiffner_val[0]]*17)
+E_stiffner.extend([Estiffner_val[0]])
+E_stiffner.extend([Estiffner_val[0]]*17)
+E_stiffner.extend([Estiffner_val[0]])
+E_stiffner.extend([Estiffner_val[0]]*17)
+E_stiffner.extend([Estiffner_val[0]])
+E_stiffner.extend([Estiffner_val[0]]*17)
+E_stiffner.extend([Estiffner_val[0]])
+E_stiffner.extend([Estiffner_val[0]]*17)
+E_stiffner.extend([Estiffner_val[0]])
+E_stiffner.extend([Estiffner_val[0]]*9)
+
+
+print(len(E_stiffner))
+
+#--------------------------------------------------------- setting discritization basis----------------------------------
+
+# Discretized boom parameters Stiffness and Area
 #creating boom at X,Y coordinate for discretized panel
 # calculating Boom Area
+
 T=[0.002,0.002] # placeholder, iterate later
 B_bending=[]
-
+B_areas=[]
+E_booms=[]
 for i in range(num_points):
-    sig_s1 = ((M_x*y[i-1])/Ixx_t)*T[0]
-    sig_s2 = ((M_x*y[i-num_points+1])/Ixx_t)*T[0]
-    sig_boom = ((M_x*y[i])/Ixx_t)*T[0]
+    sig_s1 = ((M_x*y[i-1])/Ixx_t)
+    sig_s2 = ((M_x*y[i-num_points+1])/Ixx_t)
+    sig_boom = ((M_x*y[i])/Ixx_t)
 
     # Boom Area = Stringer Area + skin 1 contribution + skin 2 contribution
-    Sai = A_stiffner[0]
-    B1i = (T[0]*arc_length[0]/6)*(2+(sig_s1/sig_boom))
-    B2i = (T[0]*arc_length[0]/6)*(2+(sig_s2/sig_boom))
+    Sai = stiff_loc[i]
+    B1i = (T_skinarr[i-1]*arc_length/6)*(2+(sig_s1/sig_boom))
+    B2i = (T_skinarr[i]*arc_length/6)*(2+(sig_s2/sig_boom))
     B_area = Sai+B1i+B2i
     Ixx_boom = (y[i]**2)*B_area
     B_bending.append(Ixx_boom)
+    B_areas.append(B_area)
+    E_boom=((E_skin[i]*Panel_Areas[i])+(E_skin[i-1]*Panel_Areas[i-1])+(E_stiffner[i]*stiff_loc[i]))/(stiff_loc[i]+Panel_Areas[i]+Panel_Areas[i-1])
+    E_booms.append(E_boom)
 
+# calculate stress due to moment and shear on the discritised length
+sig_z=[]
+delQ=[]
 Ixx_full=np.sum(B_bending)
-sig_z=M_x*y/Ixx_boom
+for i in range(num_points):
+    sig_boom=M_x*y[i]/Ixx_full
+    delQi=-(V_y/Ixx_full)*B_areas[i]*y[i]
+    sig_z.append(sig_boom/1e6)
+    delQ.append(delQi)
 
-i=2
-print(sig_z[i]/1e6)
+shear_val=np.zeros(num_points)
+sum=0
+for i in range(num_points):
+    sum += delQ[i-int((3/4)*num_points)]
+    shear_val[i-int((3/4)*num_points)+1]=sum
+
+sig_skin=[]
+skin1contri=[]
+skin2contri=[]
+sig_stiffner=[]
+for i in range(num_points):
+    sig_boom=sig_z[i]
+    sig_skin1 = (E_skin[i-1]/E_booms[i])*sig_boom
+    sig_skin2 = (E_skin[i]/E_booms[i])*sig_boom
+    sig_stiff = (E_stiffner[i]/E_booms[i])*sig_boom
+    sig_stiffner.append(sig_stiff)
+    skin1contri.append(sig_skin1)
+    skin2contri.append(sig_skin2)
+
+for i in range(num_points):
+    sigmaval=skin1contri[i]+((skin1contri[i]-skin2contri[i-1])/2)
+    sig_skin.append(sigmaval)
+
+i=0#int(num_points/4)
+print(sig_z[i])#(arc_length[i]*1e6))
+print(shear_val[i])
+print(sig_skin[i])
+
+plt.scatter(x[0:9],y[0:9],color='blue',marker='o',s=33)
+plt.scatter(x[9:27],y[9:27],color='red',marker='x',s=66)
+plt.scatter(x[27:45],y[27:45],color='black',marker='o',s=100)
+plt.scatter(x[45:63],y[45:63],color='red',marker='x',s=66)
+plt.scatter(x[63:81],y[63:81],color='blue',marker='o',s=33)
+plt.scatter(x[81:99],y[81:99],color='red',marker='x',s=66)
+plt.scatter(x[99:117],y[99:117],color='black',marker='o',s=100)
+plt.scatter(x[117:135],y[117:135],color='red',marker='x',s=66)
+plt.scatter(x[135:144],y[135:144],color='blue',marker='o',s=33)
 plt.scatter(x[i],y[i],color='black',marker='o',s=150)
 plt.scatter(x[i-num_points+1],y[i-num_points+1],color='blue',marker='o',s=150)
 plt.scatter(x[i-1],y[i-1],color='red',marker='o',s=150)
+plt.scatter(X[i],Y[i],color='green',marker='x')
 plt.show()
